235d9b140dd9eef65de381e75156909acf8d030b
boenwip <74122285+boenwip@users.noreply.github.com>
2021-03-08 17:57:26 +1000

    Update ssh_loop.sh

--- Linux/ssh_loop.sh
+++ Linux/ssh_loop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# List of hosts on vnet
 hosts=('10.0.0.6'
         '10.0.0.7'
         '10.0.0.9')
 
+# Set value to false for boolean to not be true.
+# Creates infinite loop attempting ssh logins through $hosts list
 flag=false
 while  [ flag != true ]
 do
